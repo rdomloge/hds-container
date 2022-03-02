7c2c0c01eb4d876d2d34d6674e7d7325794e4dec
Ramsay Domloge <rdomloge@gmail.com>
2022-03-02 22:20:22 +0000

    Fixed issue with builds doing nothing because of local cache

--- build-multiarch.bat
+++ build-multiarch.bat
@@ -1,1 +1,2 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t rdomloge/hds-container:v1.2 . --push
+#docker buildx build --platform linux/amd64,linux/arm64 -t rdomloge/hds-container:v1.2 . --push
+docker build --no-cache -t rdomloge/hds-container:v1.2 .
